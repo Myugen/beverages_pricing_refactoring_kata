ec95a1e76ec439f006957acc12ba75b25b6481d6
Miguel Cabrera <miguelcabsan@gmail.com>
2021-02-24 22:33:51 +0000

    refactor(beverages): Add beverage machine builder

--- src/test/java/unit_tests/BeveragesPricingTest.java
+++ src/test/java/unit_tests/BeveragesPricingTest.java
@@ -1,9 +1,7 @@
 package unit_tests;
 
+import coffee_shop.BeverageMachine;
 import coffee_shop.menu.beverages.*;
-import coffee_shop.menu.supplements.WithCinnamon;
-import coffee_shop.menu.supplements.WithCream;
-import coffee_shop.menu.supplements.WithMilk;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -16,85 +14,85 @@ public class BeveragesPricingTest {
 
     @Test
     public void computes_coffee_price() {
-        Beverage coffee = new Coffee();
+        Beverage coffee = BeverageMachine.coffee().make();
         assertThat(coffee.price(), is(closeTo(1.20, PRECISION)));
     }
 
     @Test
     public void computes_tea_price() {
-        Beverage tea = new Tea();
+        Beverage tea = BeverageMachine.tea().make();
         assertThat(tea.price(), is(closeTo(1.50, PRECISION)));
     }
 
     @Test
     public void computes_hot_chocolate_price() {
-        Beverage hotChocolate = new HotChocolate();
+        Beverage hotChocolate = BeverageMachine.hotChocolate().make();
         assertThat(hotChocolate.price(), is(closeTo(1.45, PRECISION)));
     }
 
     @Test
     public void computes_tea_with_cinnamon_price() {
-        Beverage teaWithCinnamon = new WithCinnamon(new Tea());
+        Beverage teaWithCinnamon = BeverageMachine.tea().withCinnamon().make();
         assertThat(teaWithCinnamon.price(), is(closeTo(1.55, PRECISION)));
     }
 
     @Test
     public void computes_tea_with_milk_price() {
-        Beverage teaWithMilk = new WithMilk(new Tea());
+        Beverage teaWithMilk = BeverageMachine.tea().withMilk().make();
         assertThat(teaWithMilk.price(), is(closeTo(1.60, PRECISION)));
     }
 
     @Test
     public void computes_tea_with_milk_and_cinnamon_price() {
-        Beverage teaWithMilkAndCinnamon = new WithCinnamon(new WithMilk(new Tea()));
+        Beverage teaWithMilkAndCinnamon = BeverageMachine.tea().withMilk().withCinnamon().make();
         assertThat(teaWithMilkAndCinnamon.price(), is(closeTo(1.65, PRECISION)));
     }
 
     @Test
     public void computes_coffee_with_cinnamon_price() {
-        Beverage coffeeWithCinnamon = new WithCinnamon(new Coffee());
+        Beverage coffeeWithCinnamon = BeverageMachine.coffee().withCinnamon().make();
         assertThat(coffeeWithCinnamon.price(), is(closeTo(1.25, PRECISION)));
     }
 
     @Test
     public void computes_coffee_with_milk_price() {
-        Beverage coffeeWithMilk = new WithMilk(new Coffee());
+        Beverage coffeeWithMilk = BeverageMachine.coffee().withMilk().make();
         assertThat(coffeeWithMilk.price(), is(closeTo(1.30, PRECISION)));
     }
 
     @Test
     public void computes_coffee_with_milk_and_cinnamon_price() {
-        Beverage coffeeWithMilkAndCinnamon = new WithCinnamon(new WithMilk(new Coffee()));
+        Beverage coffeeWithMilkAndCinnamon = BeverageMachine.coffee().withMilk().withCinnamon().make();
         assertThat(coffeeWithMilkAndCinnamon.price(), is(closeTo(1.35, PRECISION)));
     }
 
     @Test
     public void computes_coffee_with_milk_and_cream_price() {
-        Beverage coffeeWithMilkAndCream = new WithCream(new WithMilk(new Coffee()));
+        Beverage coffeeWithMilkAndCream = BeverageMachine.coffee().withMilk().withCream().make();
         assertThat(coffeeWithMilkAndCream.price(), is(closeTo(1.45, PRECISION)));
     }
 
     @Test
     public void computes_coffee_with_milk_and_cream_and_cinnamon_price() {
-        Beverage coffeeWithMilkAndCreamAndCinnamon = new WithCinnamon(new WithCream(new WithMilk(new Coffee())));
+        Beverage coffeeWithMilkAndCreamAndCinnamon = BeverageMachine.coffee().withMilk().withCream().withCinnamon().make();
         assertThat(coffeeWithMilkAndCreamAndCinnamon.price(), is(closeTo(1.5, PRECISION)));
     }
 
     @Test
     public void computes_hot_chocolate_with_cinnamon_price() {
-        Beverage hotChocolateWithCinnamon = new WithCinnamon(new HotChocolate());
+        Beverage hotChocolateWithCinnamon = BeverageMachine.hotChocolate().withCinnamon().make();
         assertThat(hotChocolateWithCinnamon.price(),  is(closeTo(1.5, PRECISION)));
     }
 
     @Test
     public void computes_hot_chocolate_with_cream_price() {
-        Beverage hotChocolateWithCream = new WithCream(new HotChocolate());
+        Beverage hotChocolateWithCream = BeverageMachine.hotChocolate().withCream().make();
         assertThat(hotChocolateWithCream.price(),  is(closeTo(1.60, PRECISION)));
     }
 
     @Test
     public void computes_hot_chocolate_with_cream_and_cinnamon_price() {
-        Beverage hotChocolateWithCreamAndCinnamon = new WithCinnamon(new WithCream(new HotChocolate()));
+        Beverage hotChocolateWithCreamAndCinnamon = BeverageMachine.hotChocolate().withCream().withCinnamon().make();
         assertThat(hotChocolateWithCreamAndCinnamon.price(),  is(closeTo(1.65, PRECISION)));
     }
 }
