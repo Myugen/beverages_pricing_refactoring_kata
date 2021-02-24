3f6b08584adcf78ef6e347d42a409d22172782e6
Miguel Cabrera <miguelcabsan@gmail.com>
2021-02-24 22:12:45 +0000

    refactor(tests): Refactor tests to uses supplements decorators

--- src/test/java/unit_tests/BeveragesPricingTest.java
+++ src/test/java/unit_tests/BeveragesPricingTest.java
@@ -1,6 +1,8 @@
 package unit_tests;
 
 import beverages.*;
+import beverages.suplements.WithCream;
+import beverages.suplements.WithMilk;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -8,45 +10,48 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.closeTo;
 
 public class BeveragesPricingTest {
+
+    public static final double PRECISION = 0.001;
+
     @Test
     public void computes_coffee_price() {
         Beverage coffee = new Coffee();
-        assertThat(coffee.price(), is(closeTo(1.20, 0.001)));
+        assertThat(coffee.price(), is(closeTo(1.20, PRECISION)));
     }
 
     @Test
     public void computes_tea_price() {
         Beverage tea = new Tea();
-        assertThat(tea.price(), is(closeTo(1.50, 0.001)));
+        assertThat(tea.price(), is(closeTo(1.50, PRECISION)));
     }
 
     @Test
     public void computes_hot_chocolate_price() {
         Beverage hotChocolate = new HotChocolate();
-        assertThat(hotChocolate.price(), is(closeTo(1.45, 0.001)));
+        assertThat(hotChocolate.price(), is(closeTo(1.45, PRECISION)));
     }
 
     @Test
     public void computes_tea_with_milk_price() {
-        Tea teaWithMilk = new TeaWithMilk();
-        assertThat(teaWithMilk.price(), is(closeTo(1.60, 0.001)));
+        Beverage teaWithMilk = new WithMilk(new Tea());
+        assertThat(teaWithMilk.price(), is(closeTo(1.60, PRECISION)));
     }
 
     @Test
     public void computes_coffee_with_milk_price() {
-        Coffee coffeeWithMilk = new CoffeeWithMilk();
-        assertThat(coffeeWithMilk.price(), is(closeTo(1.30, 0.001)));
+        Beverage coffeeWithMilk = new WithMilk(new Coffee());
+        assertThat(coffeeWithMilk.price(), is(closeTo(1.30, PRECISION)));
     }
 
     @Test
     public void computes_coffee_with_milk_and_cream_price() {
-        Coffee coffeeWithMilkAndCream = new CoffeeWithMilkAndCream();
-        assertThat(coffeeWithMilkAndCream.price(), is(closeTo(1.45, 0.001)));
+        Beverage coffeeWithMilkAndCream = new WithCream(new WithMilk(new Coffee()));
+        assertThat(coffeeWithMilkAndCream.price(), is(closeTo(1.45, PRECISION)));
     }
 
     @Test
     public void computes_hot_chocolate_with_cream_price() {
-        HotChocolateWithCream hotChocolateWithCream = new HotChocolateWithCream();
-        assertThat(hotChocolateWithCream.price(),  is(closeTo(1.60, 0.001)));
+        Beverage hotChocolateWithCream = new WithCream(new HotChocolate());
+        assertThat(hotChocolateWithCream.price(),  is(closeTo(1.60, PRECISION)));
     }
 }
